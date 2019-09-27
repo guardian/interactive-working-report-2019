5e206c1efe8cbd01976e71a8676152fba0598109
Duarte Carrilho da Graça <4561+duarte@users.noreply.github.com>
2019-09-27 15:15:23 +0200

    tweaks to section headers

--- src/js/app.js
+++ src/js/app.js
@@ -29,9 +29,11 @@ function createArticleWrapper() {
 }
 
 function createSections() {
-  var wr = document.querySelector('.working-report__wrapper');
+  let wr = document.querySelector('.working-report__wrapper');
   let wrSections = wr.querySelectorAll('blockquote');
+  let sectionNum = 0;
   wrSections.forEach(function (el) {
+    sectionNum++;
     let newSection = document.createElement('div');
     newSection.classList.add('working-report__section__inner');
     while (el.nextElementSibling != null && el.nextElementSibling.tagName != 'BLOCKQUOTE' && !el.nextElementSibling.classList.contains('working-report__section')) {
@@ -43,8 +45,29 @@ function createSections() {
     newSectionWrapper.appendChild(newSection);
     let sectionClass = el.innerText.split(' | ')[0].trim()
       .replace(/\s+/g, '-').replace('/[^a-zA-Z-]/g', '').toLowerCase();
-    newSectionWrapper.classList.add('section__' + sectionClass);
+    newSectionWrapper.classList.add(sectionClass);
+    newSectionWrapper = sectionMods(newSectionWrapper, sectionNum);
     wr.appendChild(newSectionWrapper)
 
   });
+}
+
+function sectionMods(s, n) {
+  let separator = s.querySelector('blockquote');
+  separator.classList.add('section-header');
+  let title = s.querySelector('h2');
+  let titleText = title.innerText.split(' | ');
+  title.innerHTML = '<span>' + titleText[0].split(' ').join('</span> <span>') + '</span>';
+
+  if (titleText.length > 1) {
+    let subTitle = document.createElement('h3');
+    subTitle.innerText = titleText[1];
+    separator.appendChild(subTitle);
+  }
+
+  let sectionNumber = document.createElement('div');
+  sectionNumber.classList.add('section-number');
+  sectionNumber.innerText = n;
+  separator.appendChild(sectionNumber);
+  return s;
 }
